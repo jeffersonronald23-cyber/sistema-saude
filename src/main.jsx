42fa7d4810e213019220ab2c7e25557a135b5fd6
Jefferson <jrvarago@gmail.com>
2026-07-24 16:24:00 -0300

    Ajustando o import do App e main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css' // <-- Garanta que essa linha está aqui!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
